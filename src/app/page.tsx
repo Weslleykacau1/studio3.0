11b65fa36ebec4edb8723256e91037da4e0c5782
Weslley Sniper <weslley.kacau@gmail.com>
2025-06-30 17:50:27 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import ScriptifyStudio from "@/components/scriptify-studio";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background p-4 font-sans text-foreground sm:p-6 lg:p-8">
-      <div className="container mx-auto">
+    <main
+      className="min-h-screen bg-background p-4 font-sans text-foreground sm:p-6 lg:p-8"
+      suppressHydrationWarning
+    >
+      <div className="container mx-auto" suppressHydrationWarning>
         <ScriptifyStudio />
       </div>
     </main>
